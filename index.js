81383cd4fe8f4469bd6923a3d9e3f398d200c7d0
Richard Lenin <71629507+d-r-lenin@users.noreply.github.com>
2025-02-26 06:35:12 +0000

    update

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
+app.use("/.well-known", express.static(".well-known"));
+
 app.use("*", protectWithApiKey);
 
 app.use('/user',userRouter);
